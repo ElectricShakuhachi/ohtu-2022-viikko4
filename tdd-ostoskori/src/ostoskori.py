3b89d67f29a2df0aa407905c199a28983df6fcfb
ElectricShakuhachi <haavistoaaro@gmail.com>
2022-04-18 00:23:25 +0300

    Make ostokset attribute hidden

--- tdd-ostoskori/src/ostoskori.py
+++ tdd-ostoskori/src/ostoskori.py
@@ -3,19 +3,19 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset = {}
+        self._ostokset = {}
 
     def tavaroita_korissa(self):
-        return sum([ostos.lukumaara() for ostos in self.ostokset.values()])
+        return sum([ostos.lukumaara() for ostos in self._ostokset.values()])
 
     def hinta(self):
-        return sum([ostos.hinta() for ostos in self.ostokset.values()])
+        return sum([ostos.hinta() for ostos in self._ostokset.values()])
 
     def lisaa_tuote(self, lisattava: Tuote):
-        if lisattava.nimi not in self.ostokset:
-            self.ostokset[lisattava.nimi] = Ostos(lisattava)
+        if lisattava.nimi not in self._ostokset:
+            self._ostokset[lisattava.nimi] = Ostos(lisattava)
         else:
-            self.ostokset[lisattava.nimi].muuta_lukumaaraa(1)
+            self._ostokset[lisattava.nimi].muuta_lukumaaraa(1)
 
     def poista_tuote(self, poistettava: Tuote):
         pass
