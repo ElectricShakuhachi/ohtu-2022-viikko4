7913fe63404462be337cb56d49e88761824115a5
ElectricShakuhachi <haavistoaaro@gmail.com>
2022-04-18 00:13:35 +0300

    add test for adding item to cart

--- tdd-ostoskori/src/tests/ostoskori_test.py
+++ tdd-ostoskori/src/tests/ostoskori_test.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock, ANY
 from ostoskori import Ostoskori
 from tuote import Tuote
 
@@ -9,3 +10,9 @@ class TestOstoskori(unittest.TestCase):
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskorissa_1_tavara(self):
+        self.kori.lisaa_tuote(Tuote("Omena", 2))
+        self.assertEqual(self.kori.tavaroita_korissa(), 1)
+
+    
